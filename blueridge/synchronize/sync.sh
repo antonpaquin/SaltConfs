031c499e4b9bbbbb070c51850fdb6754b8da1da1
Anton Paquin <antonpaquin@gmail.com>
2017-01-23 12:59:22 -0500

    There was an errant hyphen, this must be corrected

--- blueridge/synchronize/sync.sh
+++ blueridge/synchronize/sync.sh
@@ -1,3 +1,3 @@
 TIMESTAMP=$(date +%Y-%m-%d.%H-%M)
-LOG_FILE="/home/pi/store/SaltLogs/{{ machine }}/{{ machine.lower() }}-$TIMESTAMP.log"
+LOG_FILE="/home/pi/store/SaltLogs/{{ machine }}/{{ machine.lower() }}.$TIMESTAMP.log"
 salt '{{ machine }}' state.apply {{ machine.lower() }} --force-color --out-file=$LOG_FILE
