c5426096cf6dc0b621b6de760ad108e1d6b031ae
Anton Paquin <antonpaquin@gmail.com>
2017-04-09 18:07:59 -0400

    I think I deleted something

--- vega/music/plexmove.sh
+++
@@ -1,5 +0,0 @@
-SRC="/home/pi/drive/torrents/What/$1"
-DEST="/home/pi/drive/Media/Music/$2 - $3"
-mv -i "$SRC" "$DEST" &&
-ln -s "$DEST" "$SRC"
-
--- vega/music/unzipToPlex.sh
+++
@@ -1,8 +0,0 @@
-TDIR="$2 - $3"
-mkdir "$TDIR"
-unzip "$1" -d "$TDIR"
-MDIR=$(echo "$1" | rev | cut -c 5- | rev)
-mv "$TDIR/$MDIR"/* "$TDIR"
-rm -r "$TDIR/$MDIR"
-rm "$1"
-mv "$TDIR" "/home/pi/drive/Media/Music/$TDIR"
